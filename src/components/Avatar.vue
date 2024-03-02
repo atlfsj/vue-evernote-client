7d0ec12f2c8c309a6d789807aaf56a753f0946d1
atlfsj <3321713512@qq.com>
2024-03-02 23:06:01 +0800

    初步完成了登录、整体页面框架

---
+++ src/components/Avatar.vue
@@ -0,0 +1,34 @@
+<template>
+    <span :title="user.username">{{slug}}</span>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                username: 'Sss'
+            },
+            slug: 'S'
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+span {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    line-height: 32px;
+    border-radius: 50%;
+    background: #f2b81c;
+    color: #fff;
+    text-shadow: 1px 0 1px #795c19;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 18px;
+    margin-top: 15px;
+}
+</style>
