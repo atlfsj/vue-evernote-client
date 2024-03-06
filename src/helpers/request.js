259efa71451928e6e02e40819ea5451688dff680
atlfsj <3321713512@qq.com>
2024-03-06 19:48:16 +0800

    修改request，写错参数了

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,16 +1,18 @@
 import axios from 'axios'
 import baseURLConfig from './config-baseURL'
 
-axios.defaults.headers.post['Content-Type']
+console.log(baseURLConfig)
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.baseURL = baseURLConfig.baseURL
-axios.defaults.withCredentials = true  // 允许跨域
+axios.defaults.withCredentials = true
 
 export default function request(url, type = 'GET', data = {}) {
     return new Promise((resolve, reject) => {
         let option = {
             url,
             method: type,
-            validityState(status) {
+            validateStatus(status) {
                 return (status >= 200 && status < 300) || status === 400
             }
         }
@@ -32,3 +34,9 @@ export default function request(url, type = 'GET', data = {}) {
         })
     })
 }
+
+
+// request('/auth/login', 'POST', {username: 'hunger', password: '123456'})
+//   .then(data=>{
+//     console.log(data)
+//   })
